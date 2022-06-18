553a58e1b79758df1ab4cadd3a4d9a8c2ddaad5f
Francisco Jesús <105810105+JesRomeGar@users.noreply.github.com>
2022-06-18 02:25:40 +0200

    Add files via upload

---
+++ Práctica 2 Usuario/Js/demo.js
@@ -0,0 +1,3 @@
+document.getElementById("nombre").value="Jesús";
+document.getElementById("apellido").value="Romero"
+document.getElementById("avatar").src="img/avatar-perfil-masculino-femenino-avatares-usuario-icono-genero_349999-700.webp"
